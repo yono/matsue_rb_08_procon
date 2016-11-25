c37eae34965c9f4c3cf66d7bc4c9980f9ddd86c5
Yusaku ONO <yono.05@gmail.com>
2016-11-25 18:07:13 +0900

    リファクタリング

--- solve.rb
+++ solve.rb
@@ -2,26 +2,13 @@ maze_ary = STDIN.each_line.map(&:chomp)
 
 # ここに処理を書いてください。
 
-class MazeCell < String
-end
-
-class MazeStart < MazeCell
-end
-
-class MazeGoal < MazeCell
-end
-
-class MazeWall < MazeCell
-end
-
-class MazeSpace < MazeCell
-end
-
-class MazeAlready < MazeCell
-end
-
-class MazeRedAlready < MazeCell
-end
+class MazeCell < String; end
+class MazeStart < MazeCell; end
+class MazeGoal < MazeCell; end
+class MazeWall < MazeCell; end
+class MazeSpace < MazeCell; end
+class MazeAlready < MazeCell; end
+class MazeRedAlready < MazeCell; end
 
 class MazeCellFactory
   def self.create(str)
@@ -38,8 +25,7 @@ class MazeCellFactory
   end
 end
 
-class MazeRow < Array
-end
+class MazeRow < Array; end
 
 class Maze < Array
   # 現在地から進める Cell がある場合に true を返す
@@ -156,6 +142,7 @@ end
 start_spot = [0, 0]
 maze_twod_ary = Maze.new
 
+# オリジナル配列を探索用配列に置き換える
 maze_ary.each_with_index do |row, row_i|
   _array = MazeRow.new
   row.enum_for(:each_char).each_with_index do |column, column_j|
@@ -167,12 +154,13 @@ maze_ary.each_with_index do |row, row_i|
   maze_twod_ary << _array
 end
 
-# random_walk
+# 探索
 current_spot = start_spot.dup
 while(!maze_twod_ary.goal?(current_spot))
   current_spot = maze_twod_ary.next_spot(current_spot)
 end
 
+# 探索結果をオリジナル配列にコピーする
 maze_twod_ary.each_with_index do |maze_row, i|
   maze_ary[i] = maze_row.join.tr('*', ' ')
 end
