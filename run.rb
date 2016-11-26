d72648944f998c72d9773e9fdf60eedb106d431d
Yusaku ONO <yono.05@gmail.com>
2016-11-27 00:29:37 +0900

    プロファイラを使うようにした

--- run.rb
+++ run.rb
@@ -20,7 +20,7 @@ expected = TEST_DATA.each_line.map(&:chomp).map { |l|
 }.join.chomp
 stdin_data = expected.gsub(":", " ")
 # stdout, status = *Open3.capture2("./solve.rb", stdin_data: stdin_data)
-stdout, status = *Open3.capture2("ruby ./solve.rb", stdin_data: stdin_data)
+stdout, status = *Open3.capture2("ruby -r profile ./solve.rb", stdin_data: stdin_data)
 stdout.gsub!(/((\r)?\n)*\z/m, "")
 
 puts(<<EOS)
