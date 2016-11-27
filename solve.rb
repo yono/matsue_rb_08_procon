937b5b73844edab59c85978b2bd03130a6e8c8b8
Yusaku ONO <yono.05@gmail.com>
2016-11-27 18:20:44 +0900

    配列の長さをキャッシュする

--- solve.rb
+++ solve.rb
@@ -3,14 +3,14 @@ maze_ary = STDIN.each_line.map(&:chomp)
 # ここに処理を書いてください。
 
 class Maze
-  def self.goal?(i, j, maze_ary)
+  def self.goal?(i, j, i_max, j_max, maze_ary)
     if i > 0
       # 上探索
       if maze_ary[i-1][j] == 'G'
         return true
       end
     end
-    if i < maze_ary.length - 1
+    if i < i_max
       # 下探索
       if maze_ary[i+1][j] == 'G'
         return true
@@ -22,7 +22,7 @@ class Maze
         return true
       end
     end
-    if j < maze_ary[i].length
+    if j < j_max
       # 右探索
       if maze_ary[i][j+1] == 'G'
         return true
@@ -30,7 +30,7 @@ class Maze
     end
   end
 
-  def self.next_spot(i, j, maze_ary)
+  def self.next_spot(i, j, i_max, j_max, maze_ary)
     return_to_top = false
     return_to_bottom = false
     return_to_left = false
@@ -45,7 +45,7 @@ class Maze
         return_to_top = true
       end
     end
-    if i < maze_ary.length - 1
+    if i < i_max
       # 下探索
       bottom = maze_ary[i+1][j]
       if bottom == ' '
@@ -65,7 +65,7 @@ class Maze
         return_to_left = true
       end
     end
-    if j < maze_ary[i].length
+    if j < j_max
       # 右探索
       right = maze_ary[i][j+1]
       if right == ' '
@@ -98,9 +98,10 @@ maze_ary.each_with_index do |row, row_i|
   end
 end
 
-current_i, current_j = start_i, start_j
-while(!Maze.goal?(current_i, current_j, maze_ary))
-  current_i, current_j = Maze.next_spot(current_i, current_j, maze_ary)
+i_max = maze_ary.length - 1
+j_max = maze_ary[0].length - 1
+while(!Maze.goal?(start_i, start_j, i_max, j_max, maze_ary))
+  start_i, start_j = Maze.next_spot(start_i, start_j, i_max, j_max, maze_ary)
 end
 
 maze_ary.each_with_index do |maze_row, i|
